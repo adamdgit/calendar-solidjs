62a93e83959b498a92b791356cfbd6cf56fa79a6
adamdgit <adam_d@outlook.com>
2023-04-14 18:06:22 +1000

    Created CRUD functions for events

--- src/db/events.tsx
+++ src/db/events.tsx
@@ -6,14 +6,40 @@ type newEvent = {
   description: string,
 }
 
+// create new event
 export async function createEvent({ id, date, description }: newEvent) {
-  console.log("create event called")
+
   await db.event.create({
     data: { 
       date: date, 
       description: description,
       author: id 
     },
-  }).then(res => console.log(res)).catch(err => console.error(err))
-  
+  }).catch(err => console.error(err))
+}
+
+// delete an event by given ID
+export async function deleteEventByID(id: string) {
+
+  await db.event.delete({
+    where: { id: id }
+  }).catch(err => console.error(err))
+}
+
+// get all events by users ID
+export async function getEventsByUserID(id: string) {
+
+  const events = await db.event.findMany({
+    where: { author: id }
+  }).catch(err => console.error(err))
+  return events;
+}
+
+export async function updateEventByID(id: string, description: string) {
+
+  const events = await db.event.update({
+    where: { id: id },
+    data: { description: description }
+  }).catch(err => console.error(err))
+  return events;
 }
