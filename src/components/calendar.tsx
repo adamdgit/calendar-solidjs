412e9278c7f623513e5921ee7693b6da4f927b18
adamdgit <adam_d@outlook.com>
2023-04-25 14:36:43 +1000

    Replaced innertext reference with value ref

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,4 +1,4 @@
-import { createSignal, onMount } from "solid-js"
+import { createSignal, onMount, For, createEffect } from "solid-js"
 import { calcCalendarDays } from "~/utils/calcDays"
 import styles from "./styles.module.css"
 import DayOption from "./dayOption"
@@ -20,7 +20,7 @@ type calendarEventProps = {
   id: string;
   date: string;
   description: string;
-  authorEmail: string;
+  author: string
 }
 
 type calendarProps = {
@@ -37,71 +37,76 @@ export default function Calendar({ setPopupIsVisible, setSelectedDate, eventItem
 
   onMount(() => {
     // set current month 
-    setCalendarData(calcCalendarDays(monthSelect!.innerText, yearSelect!.innerText));
+    setCalendarData(calcCalendarDays(monthSelect!.value, yearSelect!.value));
+  })
+
+  createEffect(() => {
+    console.log(eventItems)
   })
 
   return (
-    <div class={styles.datepickerContainer} id="datepicker-container">
-      <div class={styles.datepickerCalendar}>
-        <div class={styles.datepickerHeader}>
+    <aside class={styles.right}>
+      <div class={styles.datepickerContainer} id="datepicker-container">
+        <div class={styles.datepickerCalendar}>
+          <div class={styles.datepickerHeader}>
 
-          <div class={styles.datepickerDates}>
-            <span class={styles.pickYear}>Year: 
-              <select ref={yearSelect} onChange={() => setCalendarData(calcCalendarDays(monthSelect!.innerText, yearSelect!.innerText))}>
-                {
-                  yearData.map(year => (
-                    <option 
-                      value={year.toString()}>
-                      {year.toString()}
-                    </option>
-                  ))
-                }
-              </select>
-            </span>
-            <span class={styles.pickMonth}>Month: 
-              <select 
-                ref={monthSelect}
-                value={new Date().getMonth()}
-                onChange={() => setCalendarData(calcCalendarDays(monthSelect!.innerText, yearSelect!.innerText))}>
-                {
-                  monthData.map((month, i) => (
-                    <option
-                      value={i}>
-                      {month}
-                    </option>
-                  ))
-                }
-              </select>
-            </span>
+            <div class={styles.datepickerDates}>
+              <span class={styles.pickYear}>Year: 
+                <select ref={yearSelect} onChange={() => setCalendarData(calcCalendarDays(monthSelect!.value, yearSelect!.value))}>
+                  {
+                    yearData.map(year => (
+                      <option 
+                        value={year.toString()}>
+                        {year.toString()}
+                      </option>
+                    ))
+                  }
+                </select>
+              </span>
+              <span class={styles.pickMonth}>Month: 
+                <select 
+                  ref={monthSelect}
+                  value={new Date().getMonth()}
+                  onChange={() => setCalendarData(calcCalendarDays(monthSelect!.value, yearSelect!.value))}>
+                  {
+                    monthData.map((month, i) => (
+                      <option
+                        value={i}>
+                        {month}
+                      </option>
+                    ))
+                  }
+                </select>
+              </span>
+            </div>
+
+            <div class={styles.datepickerDaysRow}>
+              <div class="day">Mon</div>
+              <div class="day">Tue</div>
+              <div class="day">Wed</div>
+              <div class="day">Thu</div>
+              <div class="day">Fri</div>
+              <div class="day">Sat</div>
+              <div class="day">Sun</div>
+            </div>
           </div>
 
-          <div class={styles.datepickerDaysRow}>
-            <div class="day">Mon</div>
-            <div class="day">Tue</div>
-            <div class="day">Wed</div>
-            <div class="day">Thu</div>
-            <div class="day">Fri</div>
-            <div class="day">Sat</div>
-            <div class="day">Sun</div>
-          </div>
-        </div>
-
-        <div class={styles.datepickerBody}>
-          <div class={styles.monthWrap} data-month={monthSelect?.innerText} data-year={yearSelect?.innerText} >
-          {
-            calendarData.map((day, i) => (
+          <div class={styles.datepickerBody}>
+            <div class={styles.monthWrap} data-month={monthSelect?.value} data-year={yearSelect?.value} >
+            <For each={calendarData()}>
+              {(day) =>  
               <DayOption 
                 monthSelect={monthSelect}
                 setPopupIsVisible={setPopupIsVisible}
                 setSelectedDate={setSelectedDate}
                 day={day}
                 eventItems={eventItems}
-              />
-            ))
-          }
+              />}
+            </For>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </aside>
   )
 }
