83c120f84cf91fd6d590495228fc66520e75816b
adamdgit <adam_d@outlook.com>
2023-04-29 22:40:16 +1000

    Updated Delete and Patch endpoints

--- src/routes/api/events/[id].tsx
+++ src/routes/api/events/[id].tsx
@@ -1,7 +1,8 @@
 import { APIEvent, json } from "solid-start/api";
-import { deleteEventByID, updateEventByID } from "~/db/events";
+import { deleteEventByID, getEventsByUserID, updateEventByID } from "~/db/events";
 import { getUserSession } from "~/db/session";
 
+// deletes an event by given id
 export async function DELETE({ request, params }: APIEvent) {
   const session = await getUserSession(request);
   const userId = session.get("userId");
@@ -10,17 +11,20 @@ export async function DELETE({ request, params }: APIEvent) {
   await deleteEventByID(params.id)
     .catch(err => new Response(err))
 
-  return json( request );
+  return json( true );
 }
 
+// Updates an event by given id, with given description
 export async function PATCH({ request, params }: APIEvent) {
   const session = await getUserSession(request);
   const userId = session.get("userId");
-  if (!userId) return new Response("Not logged in!", { status: 401 })
+  if (!userId) return new Response("Not logged in!", { status: 401 });
 
-  console.log(params.id)
-  await updateEventByID(params.id, 'test')
+  const bodyData = await request.json();
+  const description = bodyData.desc;
+
+  await updateEventByID(params.id, description)
     .catch(err => new Response(err))
 
-  return json( "event deleted" );
+  return json( true );
 }
