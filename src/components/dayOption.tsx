b383e4345d669f48c016d4a462cf8b30868cef2f
adamdgit <adam_d@outlook.com>
2023-06-20 18:04:20 +1000

    Fixed event counter for each day

--- src/components/dayOption.tsx
+++ src/components/dayOption.tsx
@@ -1,4 +1,4 @@
-import { createSignal, onMount } from "solid-js";
+import { createEffect, createSignal, onMount } from "solid-js";
 import styles from "./styles.module.css"
 
 type calendarEventProps = {
@@ -19,21 +19,21 @@ type dayOptionProps = {
 export default function DayOption(props: dayOptionProps) {
 
   // number of events for this day 
-  const [numEvents, setNumEvents] = createSignal<number>()
+  const [numEvents, setNumEvents] = createSignal<number>(0)
 
   function handlePopup(e: any) {
     props.setPopupIsVisible(true);
     props.setSelectedDate(e.target.value)
   }
 
-  onMount(() => {
+  createEffect(() => {
     let count = 0
     props.eventItems().map(item => {
-      if (new Date(item.date).toLocaleString('en-au') === new Date(props.day).toLocaleString('en-au')) {
+      if (new Date(item.date).toLocaleString("en-au", { day: '2-digit', month: '2-digit', year: 'numeric' }) === new Date(props.day).toLocaleString("en-au", { day: '2-digit', month: '2-digit', year: 'numeric' })) {
         count += 1
       }
     })
-    setNumEvents(0)
+    setNumEvents(count)
   })
 
   return (
